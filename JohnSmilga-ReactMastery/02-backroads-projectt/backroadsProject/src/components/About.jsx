a5fb78babf967f7b4930d92a9fb38dd5a8ddd394
Ali Munir <alimunirwaris1@gmail.com>
2024-01-19 02:02:10 +0500

    mamanging links of nav and social links in navigation bar

--- JohnSmilga-ReactMastery/02-backroads-projectt/backroadsProject/src/components/About.jsx
+++ JohnSmilga-ReactMastery/02-backroads-projectt/backroadsProject/src/components/About.jsx
@@ -27,7 +27,7 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <article href="#" className="btn">
+          <a href="#" className="btn">
             read more
           </a>
         </article>
