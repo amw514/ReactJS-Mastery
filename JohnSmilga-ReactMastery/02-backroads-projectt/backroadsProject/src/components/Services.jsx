4fa617cd078bc723612b9aa3e57c16d1c74db071
Ali Munir <alimunirwaris1@gmail.com>
2024-01-19 02:47:25 +0500

    Services and tours component completion

--- JohnSmilga-ReactMastery/02-backroads-projectt/backroadsProject/src/components/Services.jsx
+++ JohnSmilga-ReactMastery/02-backroads-projectt/backroadsProject/src/components/Services.jsx
@@ -1,3 +1,5 @@
+import { services } from "../data.js";
+
 function Services() {
   return (
     <section className="section services" id="services">
@@ -7,44 +9,19 @@ function Services() {
         </h2>
       </div>
       <div className="section-center services-center">
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-wallet fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">saving money</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-tree fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">endless hiking</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
-
-        <article className="service">
-          <span className="service-icon">
-            <i className="fas fa-socks fa-fw"></i>
-          </span>
-          <div className="service-info">
-            <h4 className="service-title">amazing comfort</h4>
-            <p className="service-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores, officia.
-            </p>
-          </div>
-        </article>
+        {services.map((service) => {
+          return (
+            <article className="service" key={service.id}>
+              <span className="service-icon">
+                <i className={service.icon}></i>
+              </span>
+              <div className="service-info">
+                <h4 className="service-title">{service.title}</h4>
+                <p className="service-text">{service.text}</p>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
