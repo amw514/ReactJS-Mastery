13d25a2d9de16313615c8c93504c141f3c9c1957
Ali Munir <alimunirwaris1@gmail.com>
2024-01-19 00:21:04 +0500

    made components for project

--- JohnSmilga-ReactMastery/02-backroads-projectt/backroadsProject/src/App.jsx
+++ JohnSmilga-ReactMastery/02-backroads-projectt/backroadsProject/src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
 
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Tours from "./components/Tours";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Tours />
+      <About />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
