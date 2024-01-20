d54ba1efe6630cf8fdf0f92e09443c034a14c593
Ali Munir <alimunirwaris1@gmail.com>
2024-01-20 22:53:23 +0500

    Update useState example to use an array and add remove and clear functionality

--- JohnSmilga-ReactMastery/03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ JohnSmilga-ReactMastery/03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,42 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const removeItems = (id) => {
+    setPeople(people.filter((person) => person.id !== id));
+  };
+
+  const clearAllItems = () => {
+    setPeople([]);
+  };
+
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button type="button" onClick={() => removeItems(id)}>
+              Remove
+            </button>
+          </div>
+        );
+      })}
+
+      <button
+        type="button"
+        className="btn"
+        onClick={clearAllItems}
+        style={{ marginTop: "2rem" }}
+      >
+        Remove all
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
