d4bf3025d7f12677d6cbbae94b3f85b55605bdf9
Ali Munir <alimunirwaris1@gmail.com>
2024-01-21 23:31:48 +0500

    Update useState object example

--- JohnSmilga-ReactMastery/03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ JohnSmilga-ReactMastery/03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
+// import { people } from "../../../data";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 25,
+    hobby: "reading books",
+  });
+
+  const displayPerson = () => {
+    setPerson({ name: "john", age: 22, hobby: "screaming at pc" });
+  };
+
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>Enjoy's : {person.hobby}</h3>
+      <button className="btn" onClick={displayPerson}>
+        show john
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
