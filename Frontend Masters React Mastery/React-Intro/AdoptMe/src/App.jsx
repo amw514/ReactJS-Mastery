e81721b70652821ce83b054c28e203ebeee93089
Ali Munir <alimunirwaris1@gmail.com>
2024-01-21 22:15:27 +0500

    Update styling and add Pet component

--- Frontend Masters React Mastery/React-Intro/AdoptMe/src/App.jsx
+++ Frontend Masters React Mastery/React-Intro/AdoptMe/src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Pet from "./Pet";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </>
+  );
 }
 
 export default App;
