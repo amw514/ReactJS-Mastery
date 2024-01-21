ced018cc5306a36293047ce31ee768ef8e1a2533
Ali Munir <alimunirwaris1@gmail.com>
2024-01-21 17:56:27 +0500

    useEffect in react

--- OtherResources/useEffect/src/main.jsx
+++ OtherResources/useEffect/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import App2 from "./App2.jsx";
 import "./index.css";
+import App3 from "./App3.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App2 />
-    {<App />}
+    {/*<App2 />*/}
+    {/*<App />*/}
+    <App3 />
   </React.StrictMode>
 );
