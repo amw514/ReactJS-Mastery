26d7f47dc24c6fe3e248e7f2d053067edc6d110c
Ali Munir <alimunirwaris1@gmail.com>
2024-01-22 22:45:21 +0500

    Update imports and add useEffect example

--- JohnSmilga-ReactMastery/03-advanced-react/src/tutorial/02-useEffect/starter/01-code-example.jsx
+++ JohnSmilga-ReactMastery/03-advanced-react/src/tutorial/02-useEffect/starter/01-code-example.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
 const CodeExample = () => {
   const [value, setValue] = useState(0);
 
+  function sayHello() {
+    console.log("heello world");
+  }
+
+  sayHello();
+
+  useEffect(() => {
+    console.log("useEffect only on first render");
+  }, []);
+
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
