58ebbf61994669200698e7c1122bef2f0f8a222c
Ali Munir <alimunirwaris1@gmail.com>
2024-01-22 06:22:06 +0500

    Add birthday reminder functionality and clear all button

--- JohnSmilga-ReactMastery/04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
+++ JohnSmilga-ReactMastery/04-fundamental-projects/01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,32 @@
+import data from "./data";
+import { useState } from "react";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [person, setPerson] = useState(data);
+
+  const removeAllItems = () => {
+    setPerson([]);
+  };
+
+  return (
+    <>
+      <h3>{person.length} birthdays today</h3>
+      <div>
+        {person.map((people) => {
+          return (
+            <div key={people.id}>
+              <h3>Name: {people.name}</h3>
+              <h4>Age: {people.age}</h4>
+              <img src={people.image} alt={people.name} className="img" />
+            </div>
+          );
+        })}
+
+        <button type="button" class="btn" onClick={removeAllItems}>
+          Clear All
+        </button>
+      </div>
+    </>
+  );
 };
 export default App;
