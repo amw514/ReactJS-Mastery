0995c3de29be9396db426f6455f4b5777a90baaf
Ali Munir <alimunirwaris1@gmail.com>
2024-01-28 10:10:10 +0500

    forms in react

--- JohnSmilga-ReactMastery/03-advanced-react/src/App.jsx
+++ JohnSmilga-ReactMastery/03-advanced-react/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from "./tutorial/03-conditional-rendering/starter/01-multiple-returns-basics";
+import Starter from "./tutorial/06-forms/starter/01-controlled-inputs";
 
 function App() {
   return (
--- JohnSmilga-ReactMastery/03-advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ JohnSmilga-ReactMastery/03-advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,47 @@
+import { useState } from "react";
+
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // onSubmit
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <h4>Controlled inputs</h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          type="text"
+          name=""
+          id="name"
+          className="form-input"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          name=""
+          id="email"
+          className="form-input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <button type="submit" className="btn btn-block">
+        Submit
+      </button>
+    </form>
+  );
 };
 export default ControlledInputs;
