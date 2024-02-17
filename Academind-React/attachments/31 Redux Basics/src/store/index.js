bc8033c0f4fdd5cd7c6f7bb632b3d2d24c532f91
Ali Munir <alimunirwaris1@gmail.com>
2024-02-17 10:00:00 +0500

    adding slice to react-redux from redux file

--- Academind-React/attachments/31 Redux Basics/src/store/index.js
+++ Academind-React/attachments/31 Redux Basics/src/store/index.js
@@ -1,5 +1,24 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: "counter",
+  initialState,
+  increment(state) {
+    state.counter++;
+  },
+  decrement(state) {
+    state.counter--;
+  },
+  increase(state, action) {
+    state.counter += action.amount;
+  },
+  toggleCounter(state) {
+    state.showCounter = !state.showCounter;
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
